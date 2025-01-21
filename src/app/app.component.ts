07ea6747daa5f7f9ef09824d36a9487c4ad9a5cf
Jean Angel <jeanangelgomez27@gmail.com>
2025-01-21 14:05:49 -0500

    fix: agregamos linea de progreso en el link skills y corregimos la barra de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,13 +30,14 @@ export class AppComponent {
     this.checkAuthentication();
   }
 
+  // Verifica si el usuario está autenticado
   checkAuthentication() {
     if (isPlatformBrowser(this.platformId)) {
-      // Solo ejecuta este código en el navegador
       this.isAuthenticated = !!localStorage.getItem('token');
     }
   }
 
+  // Maneja el logout del usuario
   logout() {
     if (isPlatformBrowser(this.platformId)) {
       localStorage.removeItem('token');
@@ -45,8 +46,15 @@ export class AppComponent {
     this.router.navigate(['/login']);
   }
 
+  // Cambia la pestaña activa y navega al endpoint correspondiente
   navigateTo(tab: string): void {
     this.activeTab = tab; // Cambiar la pestaña activa
     this.router.navigate([`/${tab}`]); // Navegar al endpoint correspondiente
   }
+
+  // Método para establecer manualmente la pestaña activa
+  setActiveTab(tab: string): void {
+    this.activeTab = tab;
+  }
 }
+
