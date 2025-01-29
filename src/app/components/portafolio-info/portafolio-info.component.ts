0244822f08f939cb653f0f3ac58795c65518ab3b
JeanAngel23 <jeanangelgomez27@gmail.com>
2025-01-28 23:15:17 -0500

    fix: agregando boton de instrucciones y mejorando la seccion de projects, pendiente finalizar crud y float button

--- src/app/components/portafolio-info/portafolio-info.component.ts
+++ src/app/components/portafolio-info/portafolio-info.component.ts
@@ -1,12 +1,21 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portafolio-info',
-  imports: [],
+  imports: [CommonModule,CommonModule,
+
+  ],
   templateUrl: './portafolio-info.component.html',
   styleUrl: './portafolio-info.component.css',
-  encapsulation: ViewEncapsulation.None
+  
 })
 export class PortafolioInfoComponent {
 
+  constructor(private router: Router) {}
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
 }
